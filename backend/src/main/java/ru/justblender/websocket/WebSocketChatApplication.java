ee875008ef5630491934cbc98554adb79683b9f8
justblender <justblender@ya.ru>
2020-02-04 22:42:31 +0300

    Fix indent

--- backend/src/main/java/ru/justblender/websocket/WebSocketChatApplication.java
+++ backend/src/main/java/ru/justblender/websocket/WebSocketChatApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebSocketChatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebSocketChatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketChatApplication.class, args);
+    }
 }
